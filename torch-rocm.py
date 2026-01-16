bbf4974ffa95efa9a8550d59851dec8f761c85fa
Maiser <maiseralves@gmail.com>
2026-01-16 20:38:35 -0300

    first commit

---
+++ torch-rocm.py
@@ -0,0 +1,8 @@
+import torch
+
+# Check if an AMD GPU is available and accessible by PyTorch (via ROCm)
+print(torch.cuda.is_available())
+
+# If available, get the name of the detected AMD GPU
+if torch.cuda.is_available():
+    print(torch.cuda.get_device_name(0))
